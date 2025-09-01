2b931dec1e0f220501de34f21ed6ecd98bc20fa7
ElWillyGod <manuelantognazza@gmail.com>
2025-09-01 15:40:36 -0300

    group canvas progress v2

--- canvasGroup.go
+++ canvasGroup.go
@@ -20,3 +20,41 @@ var (
 	canvasGroup = make(map[string]*CanvasGroup)
 	canvasesMu  sync.RWMutex
 )
+
+func gestCanvas(canvasID string) *CanvasGroup {
+	canvasesMu.Lock()
+	defer canvasesMu.Unlock()
+
+	if group, exits := canvasGroup[canvasID]; exits {
+		return group
+	}
+
+	group := &CanvasGroup{
+		Canvas:  initCanvas(canvasID),
+		Clients: make(map[net.Conn]bool),
+	}
+	canvasGroup[canvasID] = group
+	return group
+}
+
+func (cg *CanvasGroup) addClient(conn net.Conn) {
+	cg.Mutex.Lock()
+	defer cg.Mutex.Unlock()
+	cg.Clients[conn] = true
+}
+
+func (cg *CanvasGroup) removeClient(conn net.Conn) {
+	cg.Mutex.Lock()
+	defer cg.Mutex.Unlock()
+	delete(cg.Clients, conn)
+}
+
+func (cg *CanvasGroup) broadcast(message string, sender net.Conn) {
+	cg.Mutex.RLock()
+	defer cg.Mutex.RUnlock()
+	for client := range cg.Clients {
+		if client != sender {
+			client.Write([]byte(message))
+		}
+	}
+}
